d9a72cad235a530576438bbe85dbe22f1b00a0b0
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-24 17:37:19 +0100

    now up to date

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function ProductCard() {
+    return (
+        <div onClick={() => {console.log('clicked')}}>ProductCard</div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link"
+import ProductCard from "./components/ProductCard"
 
 export default function HomePage() {
     return (
         <main>
             <div className="">Hello world</div>
+            <div className="">
+                <ProductCard />
+            </div>
+            <div className="">
+                <Link href="/users">Users</Link>
+            </div>
         </main>
     )
 }
