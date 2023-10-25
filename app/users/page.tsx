fd7772fecac6c5bc33a543145144872853a6de61
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-25 18:44:24 +0100

    added a package for sorting

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import {sort} from 'fast-sort'
 
 type userType = {
     id: number;
@@ -15,12 +16,21 @@ export default async function UsersPage({searchParams: {sortBy}}: userPageProps)
     // const users = await fetch('https://jsonplaceholder.typicode.com/users', {'cache': 'no-cache'}) // don't cache
     // const users = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } }) // cache and then revalidate the cache every 10s
     const users: userType[] = await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+    let sorted = users
 
     if (sortBy === 'name') {
-
+        sorted = sort(sorted).asc(u => u.name);
     } else if (sortBy === 'email') {
-
+        sorted = sort(sorted).asc(u => u.email);
     }
+    /**
+        teachers solution
+        const sortedUsers = sort(users).asc(
+            sortBy === 'name'
+            ? (user) => user.name
+            : (user) => user.email
+        )
+     */
 
     return (
         <section className="p-5">
@@ -41,7 +51,7 @@ export default async function UsersPage({searchParams: {sortBy}}: userPageProps)
             </div>
             <div className="">
                 {
-                    users.map(ech => {
+                    sorted.map(ech => {
                         return (
                             <div key={`${ech.id}-${ech.username}`} className="flex justify-between py-4 border-b font-medium">
                                 <div className="w-2/4">{ech.name}</div>
